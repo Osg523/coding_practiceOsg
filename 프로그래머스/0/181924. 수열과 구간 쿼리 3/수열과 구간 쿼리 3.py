7c34c7fabf9f25ffb15fcb9db7aeb5df48b4d8d1
오성균 <osg021015@gmail.com>
2025-09-28 00:35:33 +0900

    Update 수열과 구간 쿼리 3.py

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -10,3 +10,12 @@ def solution(arr, queries):
     for i in arr:
         answer.append(i)
     return answer
+
+# 다른 사람이 푼 코드 배울 점 
+# def solution(arr, queries):
+#     answer = []
+#     for a,b in queries:
+#         arr[a], arr[b] = arr[b], arr[a]
+            
+#     answer =arr
+#     return answer
