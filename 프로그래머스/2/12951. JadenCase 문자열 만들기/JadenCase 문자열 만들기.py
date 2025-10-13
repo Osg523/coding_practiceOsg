8e9973bad4d6c14d39457066ed2621feffce7005
오성균 <osg021015@gmail.com>
2025-10-14 00:23:06 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.07 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = ''
+    answer += s[0].upper()
+    for i in range(1, len(s)):
+        if s[i] != ' ':
+            if s[i-1] == ' ':
+                answer += s[i].upper()
+            else:
+                answer += s[i].lower()
+        else:
+            if s[i-1] == ' ':
+                answer += s[i]
+            elif s[i-1] != ' ':
+                answer += s[i]
+
+    return answer
