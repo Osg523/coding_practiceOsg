52c09786b5d7b9529cc299cb7042d5701392aa0a
오성균 <osg021015@gmail.com>
2025-11-03 23:43:52 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.26 ms, Memory: 9.12 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    a = 1
+    b = 2
+    for i in range(3,n+1):
+        a, b = b, a+b
+    answer = b % 1234567
+    return answer
