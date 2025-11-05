af792bcf374ddcf87b82e88ba93605e81069221e
오성균 <osg021015@gmail.com>
2025-11-05 23:11:20 +0900

    Update 정수 제곱근 판별.py

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -7,3 +7,15 @@ def solution(n):
     else:
         answer = -1
     return answer
+
+
+""" math 함수 사용 안하고 싶을 때 코드
+def solution(n):
+    answer = 0
+    x = n**0.5
+    if x == int(x):
+        answer = (x+1)**2
+    else:
+        answer = -1
+    return answer
+"""
