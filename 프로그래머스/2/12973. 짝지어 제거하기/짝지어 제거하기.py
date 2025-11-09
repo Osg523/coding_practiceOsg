e9ef94ace16b8637036db832f9715292c2ded49e
오성균 <osg021015@gmail.com>
2025-11-09 22:42:35 +0900

    Refactor solution for pair removal logic
    Refactor solution function to remove pop function and use a stack-like approach.

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -16,3 +16,24 @@ def solution(s):
         answer = 0
         
     return answer
+    
+''' pop함수 없이
+def solution(s):
+
+    answer = 0
+    a = list(s)
+    top = -1
+    i = 0
+
+    while i < len(a):
+        if top >= 0 and a[top] == a[i]:
+            top -= 1
+        else:
+            top += 1
+            a[top] = a[i]
+        i += 1
+    if top == -1:
+        answer = 1
+
+    return answer
+'''
