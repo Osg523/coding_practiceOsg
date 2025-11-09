2a223856062237e955e0b950a2c5aa51b6c3687f
오성균 <osg021015@gmail.com>
2025-11-09 22:41:24 +0900

    [level 2] Title: 짝지어 제거하기, Time: 259.90 ms, Memory: 15.3 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,17 +1,18 @@
 def solution(s):
-    if len(s) & 1:
-        return 0
-
-    a = list(s)  
-    top = -1        
-    i = 0            
-
-    while i < len(a):
-        if top >= 0 and a[top] == a[i]:
-            top -= 1           
-        else:
-            top += 1            
-            a[top] = a[i]       
-        i += 1
-
-    return 1 if top == -1 else 0
+    answer = -1
+    a = 0
+    list1 = []
+    for i in s:
+        list1.append(i)
+        if len(list1) > 1 and list1[a-1] == list1[a]:
+            list1.pop()
+            list1.pop()
+            a -= 2
+        a += 1
+    
+    if len(list1) == 0:
+        answer = 1
+    else:
+        answer = 0
+        
+    return answer
