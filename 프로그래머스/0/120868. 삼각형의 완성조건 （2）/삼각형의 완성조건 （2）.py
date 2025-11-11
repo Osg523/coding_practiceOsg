7875b5f8b5ccbbe49479ef8d574017927720fb8b
오성균 <osg021015@gmail.com>
2025-11-11 23:58:20 +0900

    Update 삼각형의 완성조건 （2）.py

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,4 +1,8 @@
 def solution(sides):
     answer = len(range(max(sides)-min(sides),max(sides)))+len(range(max(sides)+1,sides[0]+sides[1]))
-    
     return answer
+
+'''
+def solution(sides):
+    return 2* min(sides)-1
+'''
