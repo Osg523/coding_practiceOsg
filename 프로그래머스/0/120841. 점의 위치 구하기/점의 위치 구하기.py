94446b042c0f6dad3562d70a3c6585c12d3ffee4
오성균 <osg021015@gmail.com>
2026-01-02 01:25:54 +0900

    Fix formatting and add lambda solution as comment
    Added a newline at the end of the file and commented out a lambda solution.

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -11,3 +11,5 @@ def solution(dot):
         else:
             answer = 2
     return answer
+
+#solution = lambda dot: 1 if dot[0] > 0 and dot[1] > 0 else 4 if dot[0] > 0 else 2 if dot[1] > 0 else 3
