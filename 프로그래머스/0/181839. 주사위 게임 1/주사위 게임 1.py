8f92714ced40d33a460d61f7376054fa5530b6e0
오성균 <osg021015@gmail.com>
2026-01-06 06:46:17 +0900

    Simplify condition checks in solution function

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -1,7 +1,7 @@
 def solution(a, b):
-    if (a*b)%2 == 1:
+    if (a*b)%2:
         return a**2+b**2  
-    elif (a+b)%2 == 1:
+    elif (a+b)%2:
         return 2*(a+b)
     else:
         return abs(a-b)
