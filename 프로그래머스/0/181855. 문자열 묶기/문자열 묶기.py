0136e87a4533516452c13b8a7342eaae2e7b9bc4
오성균 <osg021015@gmail.com>
2026-01-10 07:15:35 +0900

    [level 0] Title: 문자열 묶기, Time: 15.38 ms, Memory: 16.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -0,0 +1,8 @@
+def solution(strArr):
+    dic = dict()
+    for i in strArr:
+        if len(i) in dic:
+            dic[len(i)] += 1
+        else:
+            dic[len(i)] = 1
+    return max(dic.values())
