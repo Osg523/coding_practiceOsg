0285669126ff66077e6d4191fb902564ae750cab
오성균 <osg021015@gmail.com>
2026-01-10 06:51:20 +0900

    [level 0] Title: 순서쌍의 개수, Time: 53.54 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,8 +1,2 @@
-def solution(n):
-    answer = 0
-    for i in range(1,n+1):
-        if n % i == 0:
-            answer += 1
-    return answer
 
-#solution = lambda n: sum(1 for i in range(1,n+1) if n % i == 0)
+solution = lambda n: sum(1 for i in range(1,n+1) if n % i == 0)
