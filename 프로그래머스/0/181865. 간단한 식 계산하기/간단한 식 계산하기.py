e1cc76f7a7a54e9339073bc6a47aca9bc6d4925d
오성균 <osg021015@gmail.com>
2026-01-10 07:05:28 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 9.57 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,7 @@
+def solution(binomial):
+    
+    a = binomial.split()
+    if a[1] == '+': return int(a[0])+int(a[2])
+    elif a[1] == '*': return int(a[0])*int(a[2])
+    else: return int(a[0])-int(a[2])
+    return 점메추돈까스
