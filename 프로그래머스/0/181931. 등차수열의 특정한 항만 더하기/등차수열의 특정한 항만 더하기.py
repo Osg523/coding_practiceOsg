5d83c82fdefc78aa04776e8dea31c6881d65fed6
오성균 <osg021015@gmail.com>
2026-01-11 02:40:55 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.01 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,6 +1,8 @@
-def solution(a, d, included):
-    b = 0
-    for i, j in enumerate(included):
-        if j:
-            b += a + i*d
-    return b
+# def solution(a, d, included):
+#     b = 0
+#     for i, j in enumerate(included):
+#         if j:
+#             b += a + i*d
+#     return b
+
+solution = lambda a, d, included: sum(a+i*d for i,j in enumerate(included) if j)
