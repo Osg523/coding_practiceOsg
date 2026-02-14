c3afdc7e6eef302ada2453c3389e93c85ccecda3
오성균 <osg021015@gmail.com>
2026-02-15 08:07:37 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.04 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,14 +1,2 @@
 def solution(s):
-    answer = ''
-    i = 0
-    for j in s:
-        if j == ' ':
-            answer += ' '
-            i = 0
-        else:
-            if i%2:
-                answer += j.lower()
-            else:
-                answer += j.upper()
-            i += 1
-    return answer
+    return " ".join(map(lambda x: "".join([a.lower() if i % 2 else a.upper() for i, a in enumerate(x)]), s.split(" ")))
